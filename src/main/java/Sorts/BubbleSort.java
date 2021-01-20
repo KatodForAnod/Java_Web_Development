5fe214a1a34702fee8f133b2b951883849e5e585
Nikita Makarchuk <vfrfrfcbys@yandex.by>
2021-01-20 19:25:14 +0300

    added BubbleSort, InsertSort, QuickSort

---
+++ src/main/java/Sorts/BubbleSort.java
@@ -0,0 +1,31 @@
+package Sorts;
+
+public class BubbleSort {
+    public static int[] sortMinToMax(int[] array) {
+        if (array.length < 2) {
+            if (array.length == 0) {
+                return null;
+            }
+
+            return array;
+        }
+
+        int tmp;
+        int sizeOfMass = array.length;
+
+        while (sizeOfMass > 1) {
+            for (int i = 1; i < sizeOfMass; i++) {
+                if (array[i - 1] > array[i]) {
+                    tmp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = tmp;
+                }
+            }
+
+            sizeOfMass--;
+        }
+
+
+        return array;
+    }
+}
---
+++ src/main/java/Sorts/QuickSort.java
@@ -0,0 +1,49 @@
+package Sorts;
+
+public class QuickSort {
+    public static int[] sortMinToMax(int[] source, int leftBorder, int rightBorder) {
+        if (source.length < 2) {
+            if (source.length == 0) {
+                return null;
+            }
+
+            return source;
+        }
+
+        int leftMarker = leftBorder;
+        int rightMarker = rightBorder;
+        int pivot = source[(leftMarker + rightMarker) / 2];
+        do {
+
+            while (source[leftMarker] < pivot) {
+                leftMarker++;
+            }
+
+            while (source[rightMarker] > pivot) {
+                rightMarker--;
+            }
+
+            if (leftMarker <= rightMarker) {
+
+                if (leftMarker < rightMarker) {
+                    int tmp = source[leftMarker];
+                    source[leftMarker] = source[rightMarker];
+                    source[rightMarker] = tmp;
+                }
+
+                leftMarker++;
+                rightMarker--;
+            }
+        } while (leftMarker <= rightMarker);
+
+
+        if (leftMarker < rightBorder) {
+            sortMinToMax(source, leftMarker, rightBorder);
+        }
+        if (leftBorder < rightMarker) {
+            sortMinToMax(source, leftBorder, rightMarker);
+        }
+
+        return source;
+    }
+}
