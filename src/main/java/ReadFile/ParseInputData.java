ad0034a3eb568090709e73154265c802097c4c75
Nikita Makarchuk <vfrfrfcbys@yandex.by>
2021-01-29 01:52:12 +0300

    created static field patternString for matches

--- src/main/java/ReadFile/ParseInputData.java
+++ src/main/java/ReadFile/ParseInputData.java
@@ -7,10 +7,11 @@ import java.util.regex.Pattern;
 
 public class ParseInputData {
     private static final Logger logger = Logger.getLogger(ParseInputData.class);
+    private static final String patternString = "[a-zA-Z]";
 
     public static boolean onlyIntCheck(String inputData) {
         logger.info("started method onlyIntCheck");
-        Pattern pattern = Pattern.compile("[a-zA-Z]");
+        Pattern pattern = Pattern.compile(patternString);
         Matcher matcher = pattern.matcher(inputData);
 
         return !matcher.find();
