592bde4dbe0434e7ff058e29fd90e4f9dfd1dfff
Nikita Makarchuk <vfrfrfcbys@yandex.by>
2021-02-07 15:12:32 +0300

    added logger in DiamondStone class and field preciousness

--- src/main/java/by/makarchuk/diamondfund/entity/DiamondStone.java
+++ src/main/java/by/makarchuk/diamondfund/entity/DiamondStone.java
@@ -1,15 +1,22 @@
 package by.makarchuk.diamondfund.entity;
 
+import org.apache.log4j.Logger;
+
 import java.time.LocalDateTime;
 
 public class DiamondStone extends Stone {
+    private static final Logger logger = Logger.getLogger(DiamondStone.class);
     protected Double carats;
     protected Double transparency;
+    protected Preciousness preciousness;
 
-    public DiamondStone(LocalDateTime dateDiscovery, String name, String origin, Color color, Double carats, Double transparency) {
+    public DiamondStone(LocalDateTime dateDiscovery, String name, String origin, Color color,
+                        Double carats, Double transparency, Preciousness preciousness) {
         super(dateDiscovery, name, carats / 5, origin, color);
+        
         this.carats = carats;
         this.transparency = transparency;
+        this.preciousness = preciousness;
     }
 
     @Override
@@ -18,13 +25,18 @@ public class DiamondStone extends Stone {
     }
 
     public void setCarats(Double carats) {
+        logger.info("setCarats input: " + carats);
         weightGrm = carats / 5;
         this.carats = carats;
     }
 
     public void setTransparency(Double transparency) {
-
+        logger.info("setTransparency input: " + transparency);
         this.transparency = transparency;
+    }
+
+    public void setPreciousness(Preciousness preciousness) {
+        this.preciousness = preciousness;
     }
 
     @Override
@@ -43,6 +55,8 @@ public class DiamondStone extends Stone {
                 .append(color)
                 .append(", transparency: ")
                 .append(transparency)
+                .append(", preciousness: ")
+                .append(preciousness)
                 .append("}");
 
         return infAboutStone.toString();
