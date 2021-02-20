6288a5816d321c128d5e011340c57241ff8fb401
Nikita Makarchuk <vfrfrfcbys@yandex.by>
2021-02-20 22:23:33 +0300

    added comments

--- src/main/java/by/makarchuk/shape/validator/CheckTriangleParams.java
+++ src/main/java/by/makarchuk/shape/validator/CheckTriangleParams.java
@@ -29,7 +29,9 @@ public class CheckTriangleParams {
         return isValid;
     }*/
 
+    //проверка на одномерное представление фигуры
     public static boolean isValid1dCheck(Point a, Point b, Point c) {
+        //XYZ кол-во плоскостей
         int amountOfPlanes = 3;
         boolean isValid = false;
         if ((a.getZ() == b.getZ()) && (c.getZ() == a.getZ())) {
