d41ea952dd4b08eed4cd83a183ebd23c64c834c5
KatodForAnod <kukushkanezabudka@yandex.by>
2021-03-04 12:06:00 +0300

    init new branch

--- src/main/java/by/makarchuk/shape/factory/ShapeFactory.java
+++
@@ -1,9 +0,0 @@
-package by.makarchuk.shape.factory;
-
-import by.makarchuk.shape.entity.AbstractShape;
-import by.makarchuk.shape.entity.Point;
-import by.makarchuk.shape.exception.WrongInputParam;
-
-public interface ShapeFactory {
-    AbstractShape createShape(Point[] points) throws WrongInputParam;
-}
--- src/main/java/by/makarchuk/shape/repository/specifacation/Specification.java
+++
@@ -1,7 +0,0 @@
-package by.makarchuk.shape.repository.specifacation;
-
-import by.makarchuk.shape.entity.AbstractShape;
-
-public interface Specification {
-    boolean specify(AbstractShape abstractShape);
-}
